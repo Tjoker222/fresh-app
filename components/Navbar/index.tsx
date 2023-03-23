d97325f950be02d07a8bb2e150507fd9d03bd029
tobias57 <tobias@sthorm.io>
2023-03-23 16:05:58 -0300

    feat(navbar): add made in fresh text

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -7,17 +7,18 @@ export function Navbar() {
   ];
   return (
     <div className="flex flex-row justify-between w-full items-center px-[25rem]">
-      <img
-        src="/logo.svg"
-        class="w-12 h-12"
-        alt="the orange logo: a simple orange"
-      />
+      <div className="flex flex-row gap-x-[1rem] items-center">
+        <p className="font-inter text-white">Made in Fre$h</p>
+        <img
+          src="/logo.svg"
+          class="w-12 h-12"
+          alt="the orange logo: a simple orange"
+        />
+      </div>
       <div className=" flex flex-row gap-x-lg">
         {navLinks.map((nav) => (
           <div className="group flex flex-col gap-y-[0.05rem] hover:cursor-pointer">
-            <p className="font-inter text-black">
-              {nav.name}
-            </p>
+            <p className="font-inter text-black">{nav.name}</p>
             <div className="w-0 group-hover:w-full h-[0.01rem] bg-black ease-in duration-300" />
           </div>
         ))}
